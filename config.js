605c2571e9d19e91250cb57c1c4f4b43ca5b2db6
Dmitry Fink <dmitry@finik.net>
2012-08-16 14:51:08 -0700

    Allow comments it config.json
    Do it by using node-cjson module to read the config file

--- config.js
+++ config.js
@@ -1,15 +1,5 @@
-var fs = require('fs');
-
+var cjson = require('cjson');
 var configfile = process.argv[2] || 'config.json';
 
-try {
-	data = fs.readFileSync(configfile);
-}
-catch(err) {
-	console.log('Error opening config file: ' + err);
-	return;
-}
-
-
-module.exports = JSON.parse(data);
+module.exports = cjson.load(configfile);
 
